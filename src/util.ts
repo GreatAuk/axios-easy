a495b2f5c354771453c8a987633226c1f9c54f53
Utopia <greatauk11@gmail.com>
2025-09-24 10:26:40 +0800

    chore: 添加 util-isUndefined 函数

--- src/util.ts
+++ src/util.ts
@@ -1,2 +1,5 @@
 export const isFunction = (val: unknown): val is Function =>
   typeof val === 'function'
+
+export const isUndefined = (val: unknown): val is undefined =>
+  typeof val === 'undefined'
--- test/util.test.ts
+++ test/util.test.ts
@@ -1,26 +1,37 @@
 import { describe, it, expect } from 'vitest';
-import { isFunction } from '../src/util';
+import { isFunction, isUndefined } from '../src/util';
 
-describe('isFunction', () => {
-  it('should return true if the value is a function', () => {
-    expect(isFunction(() => { })).toBe(true);
+describe('share util functions', () => {
+  describe('isFunction', () => {
+    it('should return true if the value is a function', () => {
+      expect(isFunction(() => { })).toBe(true);
+    });
+    it('should return false if the value is not a function', () => {
+      expect(isFunction(1)).toBe(false);
+    });
+    it('should return false if the value is null', () => {
+      expect(isFunction(null)).toBe(false);
+    });
+    it('should return false if the value is undefined', () => {
+      expect(isFunction(undefined)).toBe(false);
+    });
+    it('should return false if the value is an object', () => {
+      expect(isFunction({})).toBe(false);
+    });
+    it('should return false if the value is an array', () => {
+      expect(isFunction([])).toBe(false);
+    });
+    it('should return false if the value is a string', () => {
+      expect(isFunction('')).toBe(false);
+    });
   });
-  it('should return false if the value is not a function', () => {
-    expect(isFunction(1)).toBe(false);
-  });
-  it('should return false if the value is null', () => {
-    expect(isFunction(null)).toBe(false);
-  });
-  it('should return false if the value is undefined', () => {
-    expect(isFunction(undefined)).toBe(false);
-  });
-  it('should return false if the value is an object', () => {
-    expect(isFunction({})).toBe(false);
-  });
-  it('should return false if the value is an array', () => {
-    expect(isFunction([])).toBe(false);
-  });
-  it('should return false if the value is a string', () => {
-    expect(isFunction('')).toBe(false);
+
+  describe('isUndefined', () => {
+    it('should return true if the value is undefined', () => {
+      expect(isUndefined(undefined)).toBe(true);
+    });
+    it('should return false if the value is null', () => {
+      expect(isUndefined(null)).toBe(false);
+    });
   });
 });
